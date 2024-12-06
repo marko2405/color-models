6d46510801387a7d2032c83b65886ac36d5b33b5
Marko Stojanović <stojanovicmarko688@gmail.com>
2024-12-06 12:42:02 +0100

    Added Presentation Page

--- src/pages/PresentationPage.jsx
+++ src/pages/PresentationPage.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { Typography, Box } from "@mui/material";
 
 const PresentationPage = () => (
-  <Box sx={{ textAlign: "center", marginTop: "20vh" }}>
-    <Typography variant="h4">This is Presentation Page</Typography>
+  <Box sx={{ textAlign: "center", marginTop: "0vh" }}>
+    <object
+      type="application/pdf"
+      data="src\assets\pdf\modeli_boja.pdf"
+      width="100%"
+      height="800"
+    ></object>
   </Box>
 );
 
